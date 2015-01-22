746a2eec1fc35b4b1a88e6ba3b0f28ecd367d50a
shoto <shotolee@gmail.com>
2015-01-22 18:05:14 +0800

    change images.php

--- images.php
+++ images.php
@@ -12,6 +12,8 @@
 	<ul>
 		<?php #Script 11.4-image.php
 
+		date_default_timezone_set ('Asia/Shanghai');
+
 		$dir = '../uploads';
 
 		$files = scandir($dir);
@@ -21,9 +23,13 @@
 			if (substr($image, 0, 1) != '.') {
 				$image_size = getimagesize("$dir/$image");
 
+				$file_size = round ((filesize("$dir/$image")) / 1024). "kb";
+
+				$image_date = date("F d, Y H:i:s", filemtime("$dir/$image"));
+
 				$image_name = urlencode($image);
 
-				echo "<li><a href=\"javascript:create_window('$image_name', $image_size[0], $image_size[1])\"> $image</a></li>\n";
+				echo "<li><a href=\"javascript:create_window('$image_name', $image_size[0], $image_size[1])\"> $image</a>$file_size ($image_date)</li>\n";
 			} //end of the if
 
 		} //end of the foreach loop
